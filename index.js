f09f6c30be66af5c5a6cf836f46a7b4fd3bc1857
tonitdiep <adiep123@gmail.com>
2021-03-28 22:39:37 -0400

    commit tree 909f5b9c6478a2d6ed70cc5d4098f80090adb13a

--- index.js
+++ index.js
@@ -30,63 +30,62 @@
 
 
 
-// function submitData( name, email ) {
+function submitData( name, email ) {
 
-//     let configObj = {
-//     // fetch("http://localhost:3000/users", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//             },
-//         body: JSON.stringify({
-//                 name,
-//                 email
-//             })
-//           } 
-
-//        return fetch("http://localhost:3000/users", configObj)
-
-        
-//             .then(function(response) {
-//                 return response.json()
-//                 // console.log(response)
-                
-//             })
-//             .then(function(object) {
-//                 document.body.innerHTML = object ['id']
-//                 // console.log(object)
-//             })
-//             .catch(function(error) {
-//                 document.body.innerHTML = error.message
-//                 // console.log(error)
-//             })
- 
-// }
-
-
-function submitData(name, email) {
-    // debugger
-    return fetch("http://localhost:3000/users", {
+    let configObj = {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({name, email})
-      })
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            },
+        body: JSON.stringify({
+                name,
+                email
+            })
+          } 
 
-      .then(function(response) {
-        return response.json()
+       return fetch("http://localhost:3000/users", configObj)
+
         
-      })
-      .then(function(object) {
-       console.log(object)
-        // document.body.innerHTML = object[ "id" ]
+            .then(function(response) {
+                return response.json()
+                // console.log(response)
+                
+            })
+            .then(function(object) {
+                document.body.innerHTML = object['id']
+                console.log("string", object)
+            })
+            .catch(function(error) {
+                document.body.innerHTML = error.message
+                // console.log(error)
+            })
+ 
+}
+
+
+// function submitData(name, email) {
+//     // debugger
+//     return fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify({name, email})
+//       })
+
+//       .then(function(response) {
+//         return response.json()
+        
+//       })
+//       .then(function(object) {
+//        console.log(object)
+//         // document.body.innerHTML = object[ "id" ]
    
-      })
-      .catch(function(error) {  
-        // document.body.innerHTML = error.message
-        console.log(error.message)
-      })
-  }
+//       })
+//       .catch(function(error) {  
+//         // document.body.innerHTML = error.message
+//         console.log(error.message)
+//       })
+//   }
